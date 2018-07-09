3d8bfbb1d7c9f20c2cd34cd2e4156339701e2390
Raphael Nedellec <raphael.nedellec@edf.fr>
2018-07-09 16:50:38 +0200

    package in progress

--- muchachos/R/read_data.R
+++ muchachos/R/read_data.R
@@ -1,21 +1,30 @@
 
 
 
+#' read_train_data
+#'
+#' @param file input train file
+#' @import readr, dplyr
+#' @return a data.frame
+#' @export
 read_train_data <- function(file) {
   data <- read_csv(file, col_names = TRUE,
                    col_types = paste(c("c", rep("d", 4992)), collapse = ""))
-  cm <- mapply(function(x, y) x - y, data[, -c(1, 2)], colMeans(data[, -c(1, 2)]))
-  data <- select(data, - cols_const_to_remove)     # on enleve les colonnes constantes
+  data <- select(data, - one_of(cols_const_to_remove))     # on enleve les colonnes constantes
   return(data)
 }
 
 
+#' read_test_data
+#'
+#' @param file input test file
+#' @import readr, dplyr
+#' @return a data.frame
+#' @export
 read_test_data <- function(file) {
   data <- read_csv(file, col_names = TRUE,
                    col_types = paste(c("c", rep("d", 4991)), collapse = ""))
-  cm <- mapply(function(x, y) x - y, data[, -c(1, 2)], colMeans(data[, -c(1, 2)]))
-  w <- which(colMeans(cm) == 0) # 261
-  data <- select(data, - w)     # on enleve les colonnes constantes
+  data <- select(data, - one_of(cols_const_to_remove))     # on enleve les colonnes constantes
   return(data)
 }
 
